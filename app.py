e5d96514b547d6e383017aa2f1207fb160e75eeb
Abdullah Aqeel <abdullahsallehaqeel123@gmail.com>
2025-11-18 13:51:40 +0500

    initial

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pickle
+import requests
+
+
+# -----------------------------
+# Function to fetch movie poster
+# -----------------------------
+def fetch_poster(movie_id):
+    api_key = "117c8d6e27ad7ccca41f4b79f4534b4f"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+
+# -----------------------------
+# Function to recommend movies
+# -----------------------------
+def recommend(movie):
+    index = movies_df[movies_df['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+
+    recommended_movie_names = []
+    recommended_movie_posters = []
+
+    for i in distances[1:6]:  # Top 5 recommendations
+        movie_id = movies_df.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies_df.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters
+
+
+# -----------------------------
+# Load data
+# -----------------------------
+movies_df = pickle.load(open('movies.pkl', 'rb'))  # Full DataFrame with 'title' and 'movie_id'
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies_list = movies_df['title'].values  # Titles for the selectbox
+
+# -----------------------------
+# Streamlit UI
+# -----------------------------
+st.title("🎬 Movie Recommender System")
+
+selected_movie = st.selectbox("Select a Movie", movies_list)
+
+if st.button("Show Recommendations"):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+    cols = st.columns(5)  # 5 columns for 5 recommendations
+    for col, name, poster in zip(cols, recommended_movie_names, recommended_movie_posters):
+        col.text(name)
+        col.image(poster, use_container_width=True)  # Updated parameter
